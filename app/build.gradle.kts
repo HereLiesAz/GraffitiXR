36c23086957f45ff8bc5efa4fde8518a54632aeb
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-12-03 08:37:12 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,7 +157,7 @@ dependencies {
     // ML Kit
     implementation(libs.segmentation.selfie)
 
-
+id("com.google.gms.google-services") version "4.4.4" apply false
 
     // OpenCV
     implementation(libs.opencv)
