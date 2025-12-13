20c3d7f4fed8073346493aed61e3431ca8e68937
Az <hereliesaz@gmail.com>
2025-12-13 04:16:35 -0600

    feat: Make GithubRelease and GithubAsset Parcelable
    This commit updates the `GithubRelease` and `GithubAsset` data classes to implement the `Parcelable` interface. The `@Parcelize` annotation is used to enable this functionality, allowing instances of these classes to be passed between Android components, such as through Intents.
    
    Signed-off-by: Az <hereliesaz@gmail.com>

--- app/src/main/java/com/hereliesaz/graffitixr/data/GithubRelease.kt
+++ app/src/main/java/com/hereliesaz/graffitixr/data/GithubRelease.kt
@@ -1,8 +1,11 @@
 package com.hereliesaz.graffitixr.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class GithubRelease(
     val tag_name: String,
     val name: String,
@@ -10,9 +13,10 @@ data class GithubRelease(
     val html_url: String,
     val created_at: String,
     val assets: List<GithubAsset> = emptyList()
-)
+) : Parcelable
 
 @Serializable
+@Parcelize
 data class GithubAsset(
     val browser_download_url: String
-)
+) : Parcelable
