59bf61e9223a350bd37bc4a44ba068ee5a142ebe
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-02-10 22:05:36 -0600

    Delete feature/editor/src/main/java/com/hereliesaz/graffitixr/feature/editor/EditorPanel.kt
    Signed-off-by: Az <103241502+HereLiesAz@users.noreply.github.com>

--- feature/editor/src/main/java/com/hereliesaz/graffitixr/feature/editor/EditorPanel.kt
+++
@@ -1,7 +0,0 @@
-package com.hereliesaz.graffitixr.feature.editor
-
-enum class EditorPanel {
-    NONE,
-    ADJUST, // Saturation, Brightness, Contrast, Opacity
-    COLOR   // Color Balance (R, G, B)
-}
